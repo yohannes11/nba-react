0f95f9ddac2ccd950ce94acdfb37078647bd2719
yohannes11 <johannes.neg@gmail.com>
2021-04-24 17:25:46 +0300

    before ejecting

---
+++ src/hoc/Layout/layout.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './layout.css'
+
+class Layout extends Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <div>
+                {this.props.children}
+            </div>)
+    }
+
+}
+export default Layout;
